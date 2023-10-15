d92019ab8f89d6560020c32a0e4f57b7ae9c5b40
MATRIX-feather <midnightcarnival@outlook.com>
2023-10-15 16:31:53 +0800

    rm: 移除不需要的组件

--- osu.Game.Rulesets.IGPlayer/Feature/Player/Plugins/MvisPluginProvider.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace osu.Game.Rulesets.IGPlayer.Feature.Player.Plugins
-{
-    [Obsolete("Mvis => LLin")]
-    public abstract class MvisPluginProvider : LLinPluginProvider
-    {
-    }
-}
