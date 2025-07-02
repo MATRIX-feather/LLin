5cb370cbd19c6d8f511dd77fc5b7cb9402519824
MATRIX-feather <midnightcarnival@outlook.com>
2025-07-03 00:46:30 +0800

    misc: Bump deps, bye bye NLua

--- osu.Game.Rulesets.Hikariii/Features/Player/Plugins/Internal/LuaSupport/LuaPluginProvider.cs
+++
@@ -1,9 +0,0 @@
-using osu.Game.Rulesets.Hikariii.Features.Player.Interfaces.Plugins;
-
-namespace osu.Game.Rulesets.Hikariii.Features.Player.Plugins.Internal.LuaSupport
-{
-    public class LuaPluginProvider : LLinPluginProvider
-    {
-        public override LLinPlugin CreatePlugin => new LuaPlugin();
-    }
-}
