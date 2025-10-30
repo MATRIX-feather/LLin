07385de93ba4898fd74a220dd74915a2e9b5c2ea
CloneWith <clonewith@qq.com>
2025-10-30 17:39:26 +0800

    fix: Anchor & Origin 修复

--- osu.Game.Rulesets.Hikariii/Features/Player/Graphics/SideBar/Settings/Items/SettingsListPiece.cs
+++ osu.Game.Rulesets.Hikariii/Features/Player/Graphics/SideBar/Settings/Items/SettingsListPiece.cs
@@ -19,7 +19,9 @@ namespace osu.Game.Rulesets.Hikariii.Features.Player.Graphics.SideBar.Settings.I
 
         private readonly CurrentValueText valueText = new CurrentValueText
         {
-            RelativeSizeAxes = Axes.X
+            RelativeSizeAxes = Axes.X,
+            Anchor = Anchor.CentreLeft,
+            Origin = Anchor.CentreLeft,
         };
 
         public List<T> Values { get; set; }
@@ -83,7 +85,9 @@ namespace osu.Game.Rulesets.Hikariii.Features.Player.Graphics.SideBar.Settings.I
                         Alpha = 0,
                         Font = OsuFont.GetFont(size: 20),
                         Y = -5,
-                        RelativeSizeAxes = Axes.X
+                        RelativeSizeAxes = Axes.X,
+                        Anchor = Anchor.CentreLeft,
+                        Origin = Anchor.CentreLeft,
                     };
 
                     Schedule(() => AddInternal(currentText));
