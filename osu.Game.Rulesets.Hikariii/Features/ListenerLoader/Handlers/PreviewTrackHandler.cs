2e2f5b89fef2b7857d964a09182d71e66277d51e
MATRIX-feather <midnightcarnival@outlook.com>
2026-04-03 12:10:11 +0800

    misc: fix debug build maybe

--- osu.Game.Rulesets.Hikariii/Features/ListenerLoader/Handlers/PreviewTrackHandler.cs
+++ osu.Game.Rulesets.Hikariii/Features/ListenerLoader/Handlers/PreviewTrackHandler.cs
@@ -3,15 +3,22 @@ using System.Reflection;
 using osu.Framework.Allocation;
 using osu.Framework.Bindables;
 using osu.Framework.Development;
+using osu.Framework.Graphics;
+using osu.Framework.Utils;
 using osu.Game.Audio;
 using osu.Game.Beatmaps;
 using osu.Game.Database;
+using osu.Game.Graphics;
+using osu.Game.Graphics.Sprites;
+using osu.Game.Graphics.UserInterface;
 using osu.Game.Online.API;
 using osu.Game.Online.API.Requests.Responses;
 using osu.Game.Overlays;
 using osu.Game.Overlays.Notifications;
 using osu.Game.Rulesets.Hikariii.Features.DownloadAccel;
 using osu.Game.Rulesets.Hikariii.Features.DownloadAccel.Graphics;
+using osu.Game.Tests.Visual;
+using osuTK;
 
 namespace osu.Game.Rulesets.Hikariii.Features.ListenerLoader.Handlers;
 
